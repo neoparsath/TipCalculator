0941d1f47296f03b850e6fe8a2554079a666e65d
Alvin IR <Evenwicha@gmail.com>
2021-10-22 07:32:48 +0800

    Initial commit

---
+++ app/src/main/java/com/alpinisa/calculator/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.alpinisa.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alpinisa.calculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if(cost == null) {
+            binding.tipResult.text = ""
+            return
+        }
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val tipPercentage = when (selectedId) {
+            R.id.option_1 -> 0.20
+            R.id.option_2 -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage*cost
+
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp) {
+            tip = kotlin.math.ceil(tip)
+
+        }
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+}
